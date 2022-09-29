a95f51c7b9f5d7078fe96ea25dd97d30122f8258
sriharsha02 <akshintalasriharsha@yahoo.com>
2022-09-29 23:32:07 +0530

    added pagination and order page

--- store.js
+++ store.js
@@ -1,11 +1,11 @@
 const cart_items = document.querySelector('#cart .cart-items');
-const displayCartItems = document.getElementById('displayCartInCartPageBtn')
+// const displayCartItems = document.getElementById('displayCartInCartPageBtn')
 
-displayCartItems.addEventListener('click', displayCartTable)
+// displayCartItems.addEventListener('click', displayCartTable)
   
 
 
-function displayCartTable(productList){
+function displayCartTable(){
   console.log('Inside displayCartTable function')
   axios.get('http://localhost:3001/cart')
     .then(response => {
@@ -29,27 +29,86 @@ function displayCartTable(productList){
       console.log(err)
     })
 }
+function displayOrders(){
+  console.log('inside display orders function')
+  axios.get('http://localhost:3001/orders')
+    .then(response => {
+      if(response.status === 200){
+        console.log(response)
+        response.data.orders.forEach(order => {
+          const cartOrdersContainer = document.getElementById('cartPageId');
+          cartOrdersContainer.innerHTML += `
+          <div id= 'ordersDiv'>
+          <h2><li>Item : ${order.id} is placed </li> </h2>
+          </div>
+          `
+          
+        })
+
+      }
+
+    })
+}
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  axios.get('http://localhost:3001/products').then((data) => {
-    // console.log(data);
-    if(data.request.status === 200){
-      const products = data.data;
-      console.log(products)
-      const parentSection = document.getElementById('Products');
-      products.forEach(product => {
-        const productHtml = `
-        <div id='productsdiv'>
-          <h1>${product.title}</h1>
-          <img src=${product.imageUrl}></img>
-          <button id='products' onclick='addToCart(${product.id})'>Add To Cart</button>
-        </div>`
-        parentSection.innerHTML += productHtml;
-      });
-    }
+  var path = window.location.pathname;
+  var pages = path.split("/").pop();
+  if(pages.includes('store') ){
+    const page = 1;
+    const backendAPI = 'http://localhost:3001'
+    axios.get(`${backendAPI}/products?page=${page}`)
+      .then((res) => {
+      // console.log(data);
+      listProducts(res.data.products)
+      showPagination(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    axios.get(`${backendAPI}/cart`)
+      .then((res) => res.data.products.forEach((p) => addProductToCart(p)))
+      .catch((err) => console.log(err))
+    
+  }
+  if(pages.includes('cart')){
+    displayCartTable()
+  }
+  if(pages.includes('order')){
+    displayOrders()
+  }
   })
-})
+function listProducts(productsData){
+  console.log(productsData)
+  console.log("dsjfhjksdfkjsdfkjsdfkjsdkjfkjsdfkj")
+  const products = document.getElementById('Products');
+  products.innerHTML = ''
+  productsData.forEach((product) => {
+    console.log(product)
+    console.log("dsjfhjsjsdkjfkjsdfkj")
+    const prod = document.createElement('div')
+    prod.className = "product"
+    prod.id = `product${product.id}`
+    prod.innerHTML =  `<div class='product-image'>
+      <img
+        src=${product.imageUrl}
+        alt=${product.title}
+      />
+    </div>
+    <div class="product-info">
+      <div class-"product-top-row">
+        <h3 class="product-name">${product.title}</h3>
+        <p>&#9733;&#9733;&#9733;&#9733;</p>
+      </div>
+      <div class = "product-bottom-row">
+        <p class="product-price">${product.price}</p>
+        <button class="add-to-cart" onclick='addToCart(${product.id})'>Add to Cart</button>
+      </div>
+    </div>`;
+    products.appendChild(prod)
+  })
+
+}
 
 document.addEventListener('click',(e)=>{
 
@@ -65,6 +124,20 @@ document.addEventListener('click',(e)=>{
           console.log(err);
           showNotification(err, true);
       });
+
+  }
+  if(e.target.id =='displayCartInCartPageBtn'){
+    // displayCartTable()
+    axios.post('http://localhost:3001/create-order').then(data => {
+        if(data.data.error){
+            throw new Error('Unable to buy product');
+        }
+        window.location.href = "./orders.html";
+    })
+    .catch(err => {
+        console.log(err);
+        // showNotification(err, true);
+    });
 
   }
   if (e.target.className=='cart-btn-bottom' || e.target.className=='cart-bottom' || e.target.className=='cart-holder'){
@@ -94,7 +167,8 @@ document.addEventListener('click',(e)=>{
 
 function showProductsInCart(listofproducts){
   console.log('The Produts are: ', listofproducts) 
-  cart_items.innerHTML = "";
+  const cartProductsContainer = document.getElementById('cartPageId');
+  cartProductsContainer.innerHTML = "";
   listofproducts.forEach(product => {
     console.log(product)
       const id = `shirtsId-${product.id}`;
@@ -154,4 +228,78 @@ function notifyUsers(message){
     setTimeout(() => {
       notification.remove();
     }, 2000)
+}
+function addProductToCart({
+  id,
+  title,
+  imageUrl,
+  price,
+  cartitem: {quantity},
+}){
+  const itemId = `cartitem - ${itemId}`
+  const item = document.getElementById(itemId)
+  if(item){
+    item.querySelector(".cartitem-qty").value = quantity;
+
+  }else{
+    const newItem = document.createElement("div")
+    newItem.className = "cartItem"
+    newItem.id = itemId
+    newItem.innerHTML += `<img src = ${imageUrl}`
+    newItem.innerHTML += `<p class = 'cartitem-name'>${title}</p>`
+    newItem.innerHTML += `<input class= 'cartitem-qty' type= 'number' value= ${quantity}>`
+    newItem.innerHTML += `<p class= 'cartitem-price'>${price}</p>`
+
+    const removeBtn = document.createElement('button')
+    removeBtn.innerText = "Remove"
+    removeBtn.addEventListener('click', () => {
+      const total = totalPrice.innerText;
+      let qty = newItem.querySelector(".cartitem-qty").value
+      totalPrice.innerText = parseInt(total)- parseInt(price)*parseInt(qty)
+      newItem.remove()
+    })
+    newItem.appendChild(removeBtn)
+    cartItems.appendChild(newItem)
+
+  }
+  const total  = totalPrice.innerText;
+  totalPrice.innerText = parseInt(total) + parseInt(price)
+}
+function showPagination({
+  currentPage,
+  hasNextPage,
+  nextPage,
+  hasPreviousPage,
+  previousPage,
+  lastPage
+}){
+  const showPagination = document.getElementById('Pagination');
+  showPagination.innerHTML = '';
+  if(hasPreviousPage){
+    const btn2 = document.createElement('button')
+    btn2.innerHTML = previousPage
+    btn2.addEventListener('click', () => getProducts(previousPage))
+    showPagination.appendChild(btn2)
+  }
+    const btn1 = document.createElement('button')
+    btn1.innerHTML = `<h3>${currentPage}</h3>`
+    btn1.addEventListener('click', () => getProducts(currentPage))
+    showPagination.appendChild(btn1)
+  if(hasNextPage){
+    const btn3 = document.createElement('button')
+    btn3.innerHTML = nextPage
+    btn3.addEventListener('click', () =>getProducts(nextPage))
+    showPagination.appendChild(btn3)
+  }
+
+}
+function getProducts(page){
+  axios.get(`http://localhost:3001/products?page=${page}`)
+    .then(({data : {products, ...pageData}}) => {
+      listProducts(products)
+      showPagination(pageData)
+    })
+}
+function emptyCart(){
+  cart_items.innerHTML = '';
 }
